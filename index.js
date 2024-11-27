b31bb5cbb79fedd4608cef55ac9a333a91c77057
Satepin <salva2rterranova@gmail.com>
2024-11-27 16:30:59 -0300

    readline / menu

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 "use strict";
 import readlineSync from 'readline-sync';
+import * as mainMenu from 'menus.js';
+import PromptSync from 'prompt-sync';
+let menu = 0;
+let loop = true;
+do{
+    mainMenu()
+menu = Number(readlineSync());
+switch(menu){
+
+}
+
+}while(loop);
---
+++ menus.js
@@ -0,0 +1,3 @@
+export function mainMenu(){
+    console.log('Escoja una tarea\n[1] Ver mis tareas\n[2] Buscar una tarea\n[3] Agregar una tarea\n[0] Salir\n...')
+}
