c4d172fd4b2d787c1f7e4527cdf0ce46d2647852
Satepin <salva2rterranova@gmail.com>
2024-11-27 13:18:20 -0300

    task file

---
+++ estados.js
@@ -0,0 +1,2 @@
+const estados = new Map([[1,"p","pendiente"],[2,"e","en curso"],[3,"t","terminada"],[4,"c","cancelada"]]);
+export {estados};
---
+++ task.js
@@ -0,0 +1,12 @@
+import estados from 'estados.js';
+let task = {
+    titulo : new Array(100),
+    descripcion : new Array(500),
+    status : 1,
+    fechaCreacion : null,
+    ultimaEdicion : null,
+    vencimiento : null,
+    dificultad : 1
+
+}
+export {task}
