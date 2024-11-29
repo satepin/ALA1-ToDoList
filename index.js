cf9bad585026906aa9f09ce1ce8e611306933a4a
Satepin <salva2rterranova@gmail.com>
2024-11-29 02:18:42 -0300

    ultimos fixes

--- index.js
+++ index.js
@@ -7,30 +7,35 @@ import { searchTask } from './menus/search.js';
 
 let menu = 0;
 let loop = true;
-let newTask = false;
+let newTask = null;
 const taskList = [];
 //preguntar nombre de usuario
 let username = readlineSync.question('\nCual es tu nombre?\n...');
-do{
-    console.log('Hola ' + username + '!');
+username.slice(0, 30);
+do {
+    console.log('\nHola ' + username + '!');
     mainMenu();
     menu = Number(readlineSync.question());
-    switch(menu){
+    switch (menu) {
         case 1:
-            viewMenu(taskList);
+            newTask = viewMenu(taskList);
+            if (newTask) {
+                taskMake(newTask);
+                newTask = null;
+            }
             break;
         case 2:
             newTask = searchTask(taskList, readlineSync.question('Ingrese el titulo de la tarea a buscar\n...'))
-            if(newTask){
+            if (newTask) {
                 taskMake(newTask);
-                newTask = false;
+                newTask = null;
             }
             break;
         case 3:
-            newTask = taskMake();
-            if(newTask){
+            newTask = taskMake(false);
+            if (newTask) {
                 taskList.push(newTask);
-                newTask = false;
+                newTask = null;
             } else {
                 console.log('\nSe ha cancelado la tarea\n')
             }
@@ -44,4 +49,4 @@ do{
             break;
     }
     menu = 0;
-}while(loop);
+} while (loop);
--- menus/taskMake.js
+++ menus/taskMake.js
@@ -4,51 +4,55 @@ import { menuWarning } from '../text/warning.js';
 import { makeMenu } from '../text/menus.js';
 import * as taskMakeData from '../prompt/taskMakeData.js';
 import * as mapas from '../task/mapas.js'
-import { isNewEmptyCheck , rangeCheck } from './check.js';
-export function taskMake(editTask){
-        
+import { isNewEmptyCheck, rangeCheck } from './check.js';
+
+export function taskMake(editTask) {
+
     let loop = true
     let check = true;
     let menu = 6;
     let newTask = null;
-    if(!editTask){
+    let nuevaFlag;
+    if (!editTask) {
         newTask = new task();
-    } else{
+        nuevaFlag = true;
+    } else {
         newTask = editTask;
+        nuevaFlag = false;
     }
-    
-    do{
-        makeMenu(newTask,mapas.estados,mapas.dificultades);
+
+    do {
+        makeMenu(newTask, mapas.estados, mapas.dificultades, nuevaFlag);
         menu = Number(readlineSync.question());
-        switch(menu){
+        switch (menu) {
             case 1: //nombre
-                newTask.titulo = taskMakeData.taskMakeString('el nombre','(100 caracteres maximo)',100);
+                newTask.titulo = taskMakeData.taskMakeString('el nombre', '(100 caracteres maximo)', 100);
                 newTask.ultimaEdicion = taskMakeData.lastEditDate()
                 break;
             case 2: //descripcion
-                newTask.descripcion = taskMakeData.taskMakeString('una descripcion','(500 caracteres maximo)',500);
+                newTask.descripcion = taskMakeData.taskMakeString('una descripcion', '(500 caracteres maximo)', 500);
                 newTask.ultimaEdicion = taskMakeData.lastEditDate()
                 break;
             case 3: //estado ; check de status valido
-                newTask.status = (taskMakeData.taskMakeNumber('el estado','\n[1] Pendiente\n[2] En Curso\n[3] Terminada\n[4] Cancelada'))
-                if(!rangeCheck(newTask.status,4)){ newTask.status = 1 };
-                newTask.ultimaEdicion = taskMakeData.lastEditDate()
+                newTask.status = (taskMakeData.taskMakeNumber('el estado', newTask.titulo + '\n[1] Pendiente\n[2] En Curso\n[3] Terminada\n[4] Cancelada'));
+                if (!rangeCheck(newTask.status, 4)) { newTask.status = 1 };
+                newTask.ultimaEdicion = taskMakeData.lastEditDate();
                 break;
             case 4: //Dificultad
-                newTask.dificultad = taskMakeData.taskMakeNumber('la dificultad' + '')
-                if(!rangeCheck(newTask.dificultad,3)){ newTask.dificultad = 1 };
-                newTask.ultimaEdicion = taskMakeData.lastEditDate()
+                newTask.dificultad = taskMakeData.taskMakeNumber('la dificultad', newTask.titulo + '\n[1] facil\n[2] normal\n[3] dificil');
+                if (!rangeCheck(newTask.dificultad, 3)) { newTask.dificultad = 1 };
+                newTask.ultimaEdicion = taskMakeData.lastEditDate();
                 break;
             case 5: //vencimiento
-            newTask.vencimiento = taskMakeData.taskSetDate('vencimiento',' (yyyy-mm-dd)')
-            newTask.ultimaEdicion = taskMakeData.lastEditDate()
+                newTask.vencimiento = taskMakeData.taskSetDate('vencimiento', ' (yyyy-mm-dd)', newTask.fechaCreacion);
+                newTask.ultimaEdicion = taskMakeData.lastEditDate();
                 break
             case 0: //guardar tarea
-                check = isNewEmptyCheck(newTask)
-                if(!check){
+                check = isNewEmptyCheck(newTask);
+                if (!check) {
                     loop = false;
                     return newTask;
-                }else{
+                } else {
                     console.log('\nEl atributo ' + check + ' se encuentra no definido o vacio, por favor corregir para guardar la tarea.');
                 }
                 break;
@@ -56,9 +60,9 @@ export function taskMake(editTask){
                 loop = false;
                 return false;
             default:
-                menuWarning(menu)
+                menuWarning(menu);
                 break;
         }
         menu = 6;
-    }while(loop)
+    } while (loop);
 }
